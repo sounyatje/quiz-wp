f5ddf2669660184afc76f07934a7c9772b5910bc
Sounyâ Abattouy <s.abattouy@interface3.be>
2025-02-11 17:00:16 +0100

    second commit

---
+++ quiz-wp/src/components/layout/Header.tsx
@@ -0,0 +1,19 @@
+
+import logo from '../../../public/logo.webp';
+
+
+const Header: React.FC = () => {
+
+    return (
+        <>
+        
+        <header>
+            <img src= {logo} alt="logo" className="header-logo"></img>
+        </header>
+        
+        </>
+    );
+
+};
+
+export default Header
